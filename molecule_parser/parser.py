de2c19bbf6ba58cf8f03c5cdcc190fbb40059140
Philipp Albrecht <palbrecht@mailbox.org>
2021-06-16 14:32:09 +0200

    Add test cases for provided examples

---
+++ molecule_parser/parser.py
@@ -0,0 +1,5 @@
+from typing import Dict
+
+
+def parse_molecule(formula: str) -> Dict[str, int]:
+    raise NotImplementedError
--- tests/test_molecule_parser.py
+++ tests/test_molecule_parser.py
@@ -1,5 +1,24 @@
-from molecule_parser import __version__
+import pytest
+
+from molecule_parser import __version__, parse_molecule
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+@pytest.mark.parametrize(
+    "formula,result",
+    [
+        ("H2O", {"H": 2, "O": 1}),
+        ("Mg(OH)2", {"Mg": 1, "O": 2, "H": 2}),
+        ("K4[ON(SO3)2]2", {"K": 4, "O": 14, "N": 2, "S": 4}),
+    ],
+    ids=[
+        "water",
+        "magnesium_hydroxide",
+        "fremy_salt",
+    ],
+)
+def test_parse_molecule(formula, result):
+    assert parse_molecule(formula) == result
