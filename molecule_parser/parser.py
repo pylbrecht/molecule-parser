ca6549b6c138988a927b26aafaf49823596661e6
Philipp Albrecht <palbrecht@mailbox.org>
2021-06-17 11:25:19 +0200

    Add MoleculeParser with grammar for water

--- molecule_parser/parser.py
+++ molecule_parser/parser.py
@@ -1,5 +1,37 @@
+from collections import Counter
 from typing import Dict
+
+from sly import Parser
+
+from molecule_parser.lexer import MoleculeLexer
 
 
 def parse_molecule(formula: str) -> Dict[str, int]:
     raise NotImplementedError
+
+
+class MoleculeParser(Parser):
+    """
+    atom        : atom ATOM
+                | atom NUMBER
+                | ATOM
+    """
+
+    tokens = MoleculeLexer.tokens
+
+    def __init__(self):
+        self.atoms = Counter()
+
+    @_("atom ATOM")
+    def atom(self, parser):
+        self.atoms[parser.ATOM] = 1
+
+    @_("atom NUMBER")
+    def atom(self, parser):
+        self.atoms[parser.atom] = int(parser.NUMBER)
+        return parser.atom
+
+    @_("ATOM")
+    def atom(self, parser):
+        self.atoms[parser.ATOM] = 1
+        return parser.ATOM
