cd83a5912f8ef4a2834ad59041d542965e62ddd4
Philipp Albrecht <palbrecht@mailbox.org>
2021-06-18 09:19:23 +0200

    Add docstring to parse_molecule()

--- molecule_parser/__init__.py
+++ molecule_parser/__init__.py
@@ -13,6 +13,19 @@ def create_molecule_parser() -> MoleculeParser:
 def parse_molecule(
     formula: str, parser_factory: Callable[..., IParser] = create_molecule_parser
 ) -> Dict[str, int]:
+    """
+    Parse a chemical `formula` and return a `dict` mapping each atom to its
+    count of occurrences.
+
+    :param formula: chemical formula (e.g. "H2O")
+    :param parser_factory: callable returning a molecule parser
+    :returns: `dict` mapping atoms to their occurrence count
+    :raises SyntaxError: for invalid input (bad characters, mismatched parenthesis)
+
+    >>> from molecule_parser import parse_molecule
+    >>> parse_molecule("Mg(OH)2")
+    {'Mg': 1, 'O': 2, 'H': 2}
+    """
     parser = create_molecule_parser()
     try:
         parser.validate(formula)
